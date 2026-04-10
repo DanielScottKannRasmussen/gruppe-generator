faed36d811872b83432988527ca4d0f595ed3be9
ValdeBenzonT <valdemar@treichl.dk>
2026-04-10 21:15:29 +0200

    Enhance server with JSON support and new routes
    Added JSON parsing middleware and integrated elevController for handling '/elever' routes.

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3003;
 
+app.use(express.json());
 app.use(express.static('Public'));
+
+import elevController from './controller/elevController.js';
+app.use('/elever', elevController);
 
 app.get('/', (req, res) => {
   res.sendFile('samling.html', { root: 'Public' });
